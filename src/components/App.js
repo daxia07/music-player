f163bebcfdcfa997bf21c51764c157ec78a22b09
alienz <limingxia07@gmail.com>
2021-05-25 22:36:42 +1000

    use auth0 hook for login and logout button

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import {PageLayout} from "./PageLayout";
 import MusicPlayer from "./MusicPlayer";
+import { Auth0Provider } from "@auth0/auth0-react";
+const {REACT_APP_DOMAIN, REACT_APP_AUTH0_CLIENT_ID} = process.env
 
 
 const App = () => {
     return  (
-        <PageLayout>
-            <MusicPlayer/>
-        </PageLayout>
+        <Auth0Provider
+            domain={REACT_APP_DOMAIN}
+            clientId={REACT_APP_AUTH0_CLIENT_ID}
+            redirectUri={window.location.origin}
+        >
+            <PageLayout>
+                <MusicPlayer/>
+            </PageLayout>
+        </Auth0Provider>
 )}
 
 export default App
--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,22 +1,49 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {updateData} from "../actions";
+import { useAuth0 } from "@auth0/auth0-react";
 
-//TODO: use auth0
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+    return (
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log Out
+        </button>
+    );
+};
 
 export const PageLayout = (props) => {
     const content = useSelector(state => state)
     const dispatch = useDispatch()
     const {accessToken} = content
+    const {isAuthenticated, user, isLoading} = useAuth0()
 
     useEffect(() => {
         if (!accessToken) {
             dispatch(updateData({accessToken: 'dummy'}))
         }
     });
+
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+
     return (
         <div>
-            {props.children}
+            { isAuthenticated ?
+                <div>
+                    <img src={user.picture} alt={user.name} />
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                    {props.children}
+                    <LogoutButton />
+                </div>
+                : <LoginButton /> }
         </div>
     )
-};
+}
